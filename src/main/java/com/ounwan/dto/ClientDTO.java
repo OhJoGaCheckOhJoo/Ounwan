45d9af1410f9d36fb0329196ec40422c5cbe962e
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-12-04 11:21:57 +0900

    client_Feat: #2 Client 기본 structure 구현

---
+++ src/main/java/com/ounwan/dto/ClientDTO.java
@@ -0,0 +1,27 @@
+package com.ounwan.dto;
+
+import java.sql.Date;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@Builder
+@ToString
+public class ClientDTO {
+	private String clientId;
+	private String name;
+	private String password;
+	private String email;
+	private Date birthday;
+	private String phone;
+	private String address;
+	private String addressDetail;
+	private String zipCode;
+	private int privacyTerms;
+	private boolean emailCheck;
+	private boolean activationCheck;
+	private boolean qualifiedCheck;
+	private String profileURL;
+}
