b34ff07cea778acfb03ebf06f0aa04f37326a2a5
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-12-04 16:03:39 +0900

    client_Feat: #2 - client DAO 생성

---
+++ src/main/java/com/ounwan/repository/ClientsDAO.java
@@ -0,0 +1,20 @@
+package com.ounwan.repository;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ounwan.entity.Clients;
+
+@Repository
+public class ClientsDAO {
+	
+	@Autowired
+	SqlSession sqlSession;
+	private static final String NAMESPACE = "net.ounwan.clients.";
+	
+	public int createAccount(Clients client) {
+		return sqlSession.insert(NAMESPACE + "insertClient", client);
+	}
+
+}
