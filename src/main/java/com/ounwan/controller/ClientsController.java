9848379bdca3ed488e601836e9000f9f0dd74e71
wldnjs127 <149056497+wldnjs127@users.noreply.github.com>
2023-12-04 20:52:42 +0900

    client_join_Feat: #3 - 회원가입 기본 구현

--- src/main/java/com/ounwan/controller/ClientsController.java
+++ src/main/java/com/ounwan/controller/ClientsController.java
@@ -1,5 +1,31 @@
 package com.ounwan.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ounwan.dto.ClientsDTO;
+import com.ounwan.service.ClientsService;
+
+@RestController
+@RequestMapping("/clients")
 public class ClientsController {
+	
+	@Autowired
+	ClientsService clientsService;
+	
+	@PostMapping(value = "/signUp", consumes = "application/json", produces = "text/plain;charset=UTF-8")
+	public String createAccount(@RequestBody ClientsDTO client) {
+		System.out.println(client);
+		int result = clientsService.createAccount(client);
+		if(result > 0) {
+			System.out.println("YESSSSSS");
+		} else {
+			System.out.println("NOOOOOOOOO");
+		}
+		return "success";
+	}
 
 }
--- src/main/java/com/ounwan/service/ClientsService.java
+++ src/main/java/com/ounwan/service/ClientsService.java
@@ -1,5 +1,42 @@
 package com.ounwan.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ounwan.dto.ClientsDTO;
+import com.ounwan.entity.Clients;
+import com.ounwan.repository.ClientsDAO;
+
+@Service
 public class ClientsService {
+
+	@Autowired
+	ClientsDAO clientsDAO;
+	
+	public int createAccount(ClientsDTO client) {
+		return clientsDAO.createAccount(change(client));
+	}
+	
+	public Clients change(ClientsDTO client) {
+		return Clients.builder()
+				.clientId(client.getClientId())
+				.name(client.getName())
+				.password(client.getPassword())
+				.email(client.getEmail())
+				.birthday(client.getBirthday())
+				.phone(client.getPhone())
+				.address(client.getAddress())
+				.addressDetail(client.getAddressDetail())
+				.zipCode(client.getZipCode())
+				.privacyTerms(client.getPrivacyTerms())
+				.emailCheck(false)
+				.activationCheck(true)
+				.qualifiedCheck(false)
+				.profileURL(client.getProfileURL())
+				.emailAuth(client.getEmailAuth())
+				.socialType(client.getSocialType())
+				.socialId(client.getSocialId())
+				.build();
+	}
 
 }
