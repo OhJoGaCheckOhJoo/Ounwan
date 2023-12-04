d7046059d50a18aaccb232768305d37eb05f5435
yu4923 <100287621+yu4923@users.noreply.github.com>
2023-12-04 20:51:13 +0900

    client_Feat: #2 Client 기본 setting 수정

--- src/main/java/com/ounwan/dto/ClientsDTO.java
+++ src/main/java/com/ounwan/dto/ClientsDTO.java
@@ -2,13 +2,19 @@ package com.ounwan.dto;
 
 import java.sql.Date;
 
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Getter
 @Builder
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClientsDTO {
 	private String clientId;
 	private String name;
