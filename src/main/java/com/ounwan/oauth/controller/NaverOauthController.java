fecb119b80e56ca412d8ec3ff9ebe9f00476c215
wldnjs127 <149056497+wldnjs127@users.noreply.github.com>
2023-12-07 08:51:48 +0900

    client_join_Feat:#3 - KAKAO Oatuth 구현2

--- src/main/java/com/ounwan/oauth/controller/NaverOauthController.java
+++ src/main/java/com/ounwan/oauth/controller/NaverOauthController.java
@@ -35,41 +35,41 @@ public class NaverOauthController {
 	// return type ModelAndView로 변경 필요
 	@RequestMapping(value = "/naver", produces = "text/plain;charset=utf-8")
 	public String naverLogin(@RequestParam String code, @RequestParam String state, HttpSession session)
-			throws IOException {
+			throws IOException, ParseException {
 		OAuth2AccessToken oauthToken = naverLogin.getAccessToken(session, code, state);
 		// 1. 로그인 사용자 정보를 읽어온다.
 		apiResult = naverLogin.getUserProfile(oauthToken);
+		
+		ClientsDTO naverClient = convertResult(apiResult);
+		ClientsDTO client = clientsService.checkNaverToken(naverClient.getSocialId());
 
+		if (client == null) {
+			session.setAttribute("userInfo", naverClient);
+			return "signUp";
+		}
+		session.setAttribute("userInfo", client);
+		return "login";
+	}
+	
+	public ClientsDTO convertResult(String apiResult) throws ParseException {
 		// 2. String 형식인 apiResult를 json형태로 바꿈
 		JSONParser parser = new JSONParser();
-		Object obj = null;
-		try {
-			obj = parser.parse(apiResult);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
+		Object obj = parser.parse(apiResult);
 
 		// 3. 데이터 파싱
 		JSONObject jsonObj = (JSONObject) obj;
 		JSONObject response = (JSONObject) jsonObj.get("response");
-
-		String name = (String) response.get("name");
-		String email = (String) response.get("email");
-		String phone = (String) response.get("mobile");
 		String birthday = (String) response.get("birthyear") + "-" + (String) response.get("birthday");
-		String profileURL = (String) response.get("profile_image");
-		String token = (String) response.get("id");
-
-		ClientsDTO result = clientsService.checkNaverToken(token);
-
-		if (result == null) {
-			session.setAttribute("userInfo",
-					ClientsDTO.builder().name(name).email(email).phone(phone).birthday(Date.valueOf(birthday))
-							.profileURL(profileURL).socialType("NAVER").socialId(token).build());
-			return "signUp";
-		}
-		session.setAttribute("userInfo", result);
-		return "login";
+		
+		return ClientsDTO.builder()
+				.name((String)response.get("name"))
+				.email((String)response.get("email"))
+				.phone((String)response.get("mobile"))
+				.birthday(Date.valueOf(birthday))
+				.profileURL((String)response.get("profile_image"))
+				.socialId((String)response.get("id"))
+				.socialType("KAKAO")
+				.build();
 	}
 
 }
