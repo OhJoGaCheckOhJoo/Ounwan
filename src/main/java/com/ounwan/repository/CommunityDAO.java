908f16c7496ff7460e1517ca67818aec4b5a6ebf
zhengyou628 <150200583+zhengyou628@users.noreply.github.com>
2023-12-15 11:39:35 +0900

    Community_Aeta_Feat: #16 - business logic 구현

--- src/main/java/com/ounwan/repository/CommunityDAO.java
+++ src/main/java/com/ounwan/repository/CommunityDAO.java
@@ -2,12 +2,15 @@ package com.ounwan.repository;
 
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.ounwan.entity.AetaBoards;
+import com.ounwan.entity.AetaImages;
+import com.ounwan.entity.Comments;
 
 @Repository
 public class CommunityDAO {
@@ -20,17 +23,45 @@ public class CommunityDAO {
 	public List<AetaBoards> aetaBoardList() {
 		return sqlsession.selectList(NAMESPACE + "AetaList");
 	}
-	public List<AetaBoards> aetaSearchAll(String searchWord){
-		searchWord="%"+searchWord+"%";
-		return sqlsession.selectList(NAMESPACE+"AetaSearchAll",searchWord);
+	
+	private String addPercentage(String inputValue) {
+		return "%"+inputValue+"%";
 	}
-	public List<AetaBoards> aetaSearchTitle(String searchWord){
-		searchWord="%"+searchWord+"%";
-		return sqlsession.selectList(NAMESPACE+"AetaSearchTitle",searchWord);
+	public List<AetaBoards> aetaSearchAll(String inputValue){
+		inputValue=addPercentage(inputValue);
+		return sqlsession.selectList(NAMESPACE+"AetaSearchAll",inputValue);
 	}
-	public List<AetaBoards> aetaSearchId(String searchWord){
-		searchWord= "%"+searchWord+"%";
-		return sqlsession.selectList(NAMESPACE+"AetaSearchId",searchWord);
+	public List<AetaBoards> aetaSearchTitle(String inputValue){
+		inputValue=addPercentage(inputValue);
+		return sqlsession.selectList(NAMESPACE+"AetaSearchTitle",inputValue);
+	}
+	public List<AetaBoards> aetaSearchId(String inputValue){
+		inputValue=addPercentage(inputValue);
+		return sqlsession.selectList(NAMESPACE+"AetaSearchId",inputValue);
+	}
+	public List<Map<String, Object>> aetaReadPost(int boardNumber){
+		
+		return sqlsession.selectList(NAMESPACE+"AetaReadPost", boardNumber);
+	}
+	public int aetaInsertComment(Comments comment) {
+		return sqlsession.insert(NAMESPACE+"AetaInsertComment", comment);
+	}
+	public int aetaDeleteComment(int commentId) {
+		return sqlsession.delete(NAMESPACE+"AetaDeleteComment",commentId);
+	}
+	public int aetaUpdateViews(int boardNumber) {
+		return sqlsession.update(NAMESPACE+"AetaUpdateView",boardNumber);
+	}
+	public int aetaInsertPost(AetaBoards post) {
+		System.out.println("insertPost dao");
+		return sqlsession.insert(NAMESPACE+"AetaInsertPost",post);
+	}
+	public int aetaDeletePost(int boardNumber) {
+		return sqlsession.delete(NAMESPACE+"AetaDeletePost",boardNumber);
+	}
+	
+	public int aetaInsertImageURL(AetaImages images) {
+		return sqlsession.insert(NAMESPACE+"AetaInsertImageURL",images);
 	}
 	
 }
--- src/main/java/com/ounwan/service/CommunityService.java
+++ src/main/java/com/ounwan/service/CommunityService.java
@@ -1,69 +1,124 @@
 package com.ounwan.service;
 
+import java.sql.Date;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ounwan.dto.AetaCommentsDTO;
 import com.ounwan.dto.AetaDTO;
+import com.ounwan.dto.AetaImagesDTO;
 import com.ounwan.entity.AetaBoards;
+import com.ounwan.entity.AetaImages;
+import com.ounwan.entity.Comments;
 import com.ounwan.repository.CommunityDAO;
 
 @Service
 public class CommunityService {
 
-	@Autowired 
+	@Autowired
 	CommunityDAO communityDAO;
-	
+
 	public List<AetaDTO> aetaBoardList() {
 		return changeDTOList(communityDAO.aetaBoardList());
 	}
-	public List<AetaDTO> aetaSearchAll(String searchWord){
-		return changeDTOList(communityDAO.aetaSearchAll(searchWord));
-	}
-	public List<AetaDTO> aetaSearchTitle(String searchWord){
-		return changeDTOList(communityDAO.aetaSearchTitle(searchWord));
-	}
-	public List<AetaDTO> aetaSearchId(String searchWord){
-		return changeDTOList(communityDAO.aetaSearchId(searchWord));
+
+	public List<AetaDTO> aetaSearch(String inputValue, String selectedOption) {
+
+		if (selectedOption.equals("aetaSearchAll"))
+			return changeDTOList(communityDAO.aetaSearchAll(inputValue));
+		if (selectedOption.equals("aetaSearchTitle"))
+			return changeDTOList(communityDAO.aetaSearchTitle(inputValue));
+		if (selectedOption.equals("aetaSearchId"))
+			return changeDTOList(communityDAO.aetaSearchId(inputValue));
+		System.out.println("오류");
+		return null;
+
 	}
 
-	
-	public AetaBoards changeEntity(AetaDTO aeta) {
-        return AetaBoards.builder()
-                .boardNumber(aeta.getBoardNumber())
-                .title(aeta.getTitle())
-                .contents(aeta.getContents())
-                .clientId(aeta.getClientId())
-                .createdDate(aeta.getCreatedDate())
-                .updatedDate(aeta.getCreatedDate())
-                .views(aeta.getViews())
-                .likes(aeta.getLikes())
-                .build();
-        
-    }
-	
-	public List<AetaDTO> changeDTOList(List<AetaBoards> boardList){
+	public List<Map<String, Object>> aetaReadPost(int boardNumber) {
+
+		return communityDAO.aetaReadPost(boardNumber);
+	}
+
+	public boolean aetaUpdateViews(int boardNumber) {
+		int result = communityDAO.aetaUpdateViews(boardNumber);
+		return (result > 0) ? true : false;
+	}
+
+	public int aetaInsertPost(AetaDTO post) {
+		System.out.println("insertPost service");
+		AetaBoards board = changeEntity(post);
+		int result = communityDAO.aetaInsertPost(board);		
+		System.out.println("추가된 게시글번호:"+post.getBoardNumber());
+//		if (result > 0) {
+//			images.setBoardNumber(post.getBoardNumber());
+//			System.out.println("boardNum:" + images.getBoardNumber());
+//			result=communityDAO.aetaInsertImageURL(changeEntity(images));
+//		}
+
+		return result;
+	}
+	public int aetaDeletePost(AetaDTO post) {
+		int result=communityDAO.aetaDeletePost(post.getBoardNumber());
+		return result;
+	}
+
+	public boolean aetaInsertComment(AetaCommentsDTO comment) {
+		int result = communityDAO.aetaInsertComment(changeEntity(comment));
+		return (result > 0) ? true : false;
+	}
+
+	public boolean aetaDeleteComment(AetaCommentsDTO comment) {
+		Comments c = changeEntity(comment);
+		System.out.println("boardNum: " + c.getBoardNumber());
+
+		int result = communityDAO.aetaDeleteComment(comment.getCommentNumber());
+		System.out.println(comment);
+		System.out.println(changeEntity(comment));
+
+		return (result > 0) ? true : false;
+	}
+
+	public AetaImages changeEntity(AetaImagesDTO images) {
+		return AetaImages.builder()
+				.imageUrl(images.getImageUrl())
+				.boardNumber(images.getBoardNumber())
+				.build();
+	}
+
+	public Comments changeEntity(AetaCommentsDTO comment) {
+		return Comments.builder().clientId(comment.getClient_id()).commentNumber(comment.getCommentNumber())
+				.boardNumber(comment.getBoardNumber()).contents(comment.getContents())
+				.createdDate(comment.getCreatedDate()).updatedDate(comment.getUpdatedDate()).likes(comment.getLikes())
+				.build();
+	}
+
+	public List<AetaDTO> changeDTOList(List<AetaBoards> boardList) {
 		List<AetaDTO> list = new ArrayList<AetaDTO>();
-		for(AetaBoards aeta:boardList) {
+		for (AetaBoards aeta : boardList) {
 			list.add(changeDTO(aeta));
 		}
 		return list;
-		
+
 	}
-	
-    public AetaDTO changeDTO(AetaBoards aeta) {
-    	return AetaDTO.builder()
-    			.boardNumber(aeta.getBoardNumber())
-                .title(aeta.getTitle())
-                .contents(aeta.getContents())
-                .clientId(aeta.getClientId())
-                .createdDate(aeta.getCreatedDate())
-                .updatedDate(aeta.getCreatedDate())
-                .views(aeta.getViews())
-                .likes(aeta.getLikes())
-    			.build();
-    			
-    }
+
+	public AetaDTO changeDTO(AetaBoards aeta) {
+		return AetaDTO.builder().boardNumber(aeta.getBoardNumber()).title(aeta.getTitle()).contents(aeta.getContents())
+				.clientId(aeta.getClientId()).createdDate(aeta.getCreatedDate()).updatedDate(aeta.getCreatedDate())
+				.views(aeta.getViews()).likes(aeta.getLikes()).build();
+	}
+
+	public AetaBoards changeEntity(AetaDTO aeta) {
+		return AetaBoards.builder().boardNumber(aeta.getBoardNumber()).title(aeta.getTitle())
+				.contents(aeta.getContents()).clientId(aeta.getClientId()).createdDate(aeta.getCreatedDate())
+				.updatedDate(aeta.getCreatedDate()).views(aeta.getViews()).likes(aeta.getLikes()).build();
+	}
+
+
+
 }
