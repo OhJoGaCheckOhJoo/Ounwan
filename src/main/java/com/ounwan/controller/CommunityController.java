e170cf574f9eb4249a7d79ce4f2aa4ced0145918
zhengyou628 <150200583+zhengyou628@users.noreply.github.com>
2023-12-15 11:38:17 +0900

    Community_Aeta_Feat: #13 - controller구현
    저장용. 사용 또는 참고 비추

--- src/main/java/com/ounwan/controller/CommunityController.java
+++ src/main/java/com/ounwan/controller/CommunityController.java
@@ -2,12 +2,18 @@ package com.ounwan.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ounwan.dto.AetaDTO;
+import com.ounwan.dto.ClientsDTO;
 import com.ounwan.service.CommunityService;
 
 
@@ -18,15 +24,47 @@ public class CommunityController {
 	@Autowired
 	CommunityService communityService;
 	
-	@RequestMapping("/aetaBoards")
+	@GetMapping("/aetaBoards")
 	public String aeta(Model model) {
-		System.out.println("게시판 ");
+		System.out.println("aetaBoard");
 		List<AetaDTO> aetaList = communityService.aetaBoardList();
 		model.addAttribute("aetaList", aetaList);
 		return "community/aeta/aetaBoard";
-	}	
+	}
 	
+	@GetMapping("/aetaPosting")
+	public String aetaPostingPage(Model model,HttpSession session) {
+		if(session.getAttribute("userInfo")==null) {
+			System.out.println("off to loginPage");
+			return "login";
+		}
+		return	"community/aeta/aetaPosting";	
+	}
+	
+	@GetMapping("/aetaPost")
+	public String aetaReadPost(@RequestParam String boardNumber,Model model, HttpSession session) {
+		System.out.println("ReadPost(controller)");
 		
-	
+		if(session.getAttribute("userInfo")==null) {
+			System.out.println("off to loginPage");
+			model.addAttribute("loginRequired", true);
+			//모달?
+			return "common/loginModal";
+		}else {
+			
+			ClientsDTO logedInClient = (ClientsDTO) session.getAttribute("userInfo");
+			session.setAttribute("clientId", logedInClient.getClientId());
+			
+			boolean updateView=communityService.aetaUpdateViews(Integer.parseInt(boardNumber)); //조회수 +1
+			if (updateView) {
+				System.out.println("updated views (+1)");
+			}else {
+				System.out.println("failed to update views");
+			}
+			
+			model.addAttribute("aetaPost", communityService.aetaReadPost(Integer.parseInt(boardNumber)));
+		}
+		return "community/aeta/aetaPost";
+	}
 	
 }
--- src/main/java/com/ounwan/controller/CommunityRestController.java
+++ src/main/java/com/ounwan/controller/CommunityRestController.java
@@ -2,13 +2,26 @@ package com.ounwan.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.ounwan.dto.AetaCommentsDTO;
 import com.ounwan.dto.AetaDTO;
+import com.ounwan.dto.AetaImagesDTO;
+import com.ounwan.dto.ClientsDTO;
 import com.ounwan.service.CommunityService;
 
 @RequestMapping("/community")
@@ -19,41 +32,88 @@ public class CommunityRestController {
 	CommunityService communityService;
 	
 	@GetMapping("/aetaSearch")
-	public String aetaSearchAll(String input,String selectedValue, Model model) {
-		System.out.println(selectedValue);
-		System.out.println(input);
+	public String aetaSearch(String inputValue,String selectedOption, Model model) {
+		System.out.println("selected option:"+selectedOption);
+		System.out.println("search word:"+inputValue);
+		//서비스로 조건문 옮기
 		
-		List<AetaDTO> aetaList;
-		if(selectedValue.equals("aetaSearchAll")) {
-			aetaList =communityService.aetaSearchAll(input);
-		}
-		else if(selectedValue.equals("aetaSearchTitle")) {
-			 aetaList =communityService.aetaSearchTitle(input);
-		}
-		else if(selectedValue.equals("aetaSearchId")) {
-			aetaList =communityService.aetaSearchId(input);
-		}else {
-			aetaList =null;
-			System.out.println("오류");
-		}
-			
+		List<AetaDTO> aetaList = communityService.aetaSearch(inputValue,selectedOption);
 		model.addAttribute("aetaList", aetaList);
 		return "community/aeta/aetaSearch";		
+		//return aetaList; 레컨만들면 위에 두줄 지워주기
 	}
 	
+	@PostMapping(value = "/aetaInsertComment", consumes="application/json")
+	public @ResponseBody String aetaInsertComment(@RequestBody AetaCommentsDTO comment,
+			HttpSession session) {
+		System.out.println(session.getAttribute("userInfo"));
+		ClientsDTO c= (ClientsDTO) session.getAttribute("userInfo");
+		String id=c.getClientId();
+		comment.setClient_id(id);
+		
+		boolean result = communityService.aetaInsertComment(comment);
+		System.out.println("입력완료");
+		return (result) ? "success" : "fail";
+	}
 	
-//	@GetMapping("/aetaSearchTitle")
-//	public String aetaSearchTitle(String input, Model model) {
-//		List<AetaDTO> aetaList = communityService.aetaSearchAll(input);
-//		model.addAttribute("aetaList", aetaList);
-//		return "community/aeta/aetaSearch";		
-//	}
-//	@GetMapping("/aetaSearchId")
-//	public String aetaSearchId(String input, Model model) {
-//		List<AetaDTO> aetaList = communityService.aetaSearchAll(input);
-//		model.addAttribute("aetaList", aetaList);
-//		return "community/aeta/aetaSearch";		
-//	}
+	@PostMapping(value="/aetaPosting")
+	public @ResponseBody String aetaInsertPost(@RequestBody AetaDTO obj,
+			HttpSession session) {		
+		int result=0;
+
+		ClientsDTO user=(ClientsDTO)session.getAttribute("userInfo");
+		obj.setClientId(user.getClientId());
+		System.out.println("obj:"+obj);
+		
+		result=communityService.aetaInsertPost(obj);
+		
+		return (result>0)? "success":"fail";
+	}
+	
+/*	@PostMapping(value="/aetaPosting")
+	public @ResponseBody String aetaInsertPost(
+			String title, String content,
+			@RequestPart(value="image", required=false) MultipartFile image,
+			HttpSession session) throws IllegalStateException {
+		System.out.println(content);
+		System.out.println(title);
+		System.out.println(image);
+
+		System.out.println("insertPost controller");
+//		ClientsDTO user=(ClientsDTO)session.getAttribute("userInfo");
+//		obj.setClientId(user.getClientId());
+//		
+//		
+//		boolean result=communityService.aetaInsertPost(obj);
+//		System.out.println("aeta uploaded");
+//		Í
+//		
+		//return (result)?"sucess":"fail" ;
+		return "success";
+	}
+*/
+	@DeleteMapping(value="/aetaDeletePost")
+	public @ResponseBody String aeteDeletePost(HttpSession session, 
+			@RequestBody AetaDTO post
+			) {
+			ClientsDTO user=(ClientsDTO)session.getAttribute("userInfo");
+			post.setClientId(user.getClientId());
+			
+			int result =communityService.aetaDeletePost(post);
+		
+			return (result>0)?"success":"fail";
+	}
+	
+	@DeleteMapping(value="/aetaDeleteComment",consumes="application/json" )
+	public @ResponseBody String aetaDeleteComment(@RequestBody AetaCommentsDTO comment,
+			HttpSession session) {
+		ClientsDTO c= (ClientsDTO) session.getAttribute("userInfo");
+		comment.setClient_id(c.getClientId());
+		
+		boolean result =communityService.aetaDeleteComment(comment);
+		return (result)?"success":"fail";
+	}
+
 }
 
 
