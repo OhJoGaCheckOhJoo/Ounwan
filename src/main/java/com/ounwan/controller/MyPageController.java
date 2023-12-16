8c45581c74b19088837939fa6536079dd96b1adf
dmswlb <48724085+dmswlb@users.noreply.github.com>
2023-12-16 16:22:25 +0900

    mypage_Feat: #27 - mypage controller, service, dao, entity, dto기본 구조 구현

---
+++ src/main/java/com/ounwan/controller/MyPageController.java
@@ -0,0 +1,10 @@
+package com.ounwan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/myPage")
+@Controller
+public class MyPageController {
+	
+}
---
+++ src/main/java/com/ounwan/repository/MyPageDAO.java
@@ -0,0 +1,8 @@
+package com.ounwan.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MyPageDAO {
+
+}
