f9102f658bd1ad00e975c3e0508d3dad91959566
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-12-18 19:56:36 +0900

    danggun_Feat: #26 - 공유 modal structure 구현

---
+++ src/main/webapp/resources/js/danggunShareModal.js
@@ -0,0 +1,36 @@
+// 모달 열기
+function openShareModal() {
+	document.getElementById("shareModal").style.display = "block";
+	document.getElementById("shareModalOverlay").style.display = "block";
+}
+
+// 모달 닫기
+function closeShareModal() {
+	document.getElementById("shareModal").style.display = "none";
+	document.getElementById("shareModalOverlay").style.display = "none";
+}
+
+function clipboardShare() {
+	var url = '';
+	var tmpTextarea = document.createElement("textarea");
+	var url = window.location.href;
+
+	tmpTextarea.value = url;
+
+	tmpTextarea.setAttribute("readonly", "");
+	tmpTextarea.style.position = "absolute";
+	tmpTextarea.style.left = "-9999px";
+	document.body.appendChild(tmpTextarea);
+
+	tmpTextarea.select();
+	tmpTextarea.setSelectionRange(0, 9999);
+	var successChk = document.execCommand("copy");
+
+	document.body.removeChild(tmpTextarea);
+
+	if (!successChk) {
+		alert("클립보드 복사에 실패하였습니다.");
+	} else {
+		alert("클립보드에 복사가 완료되었습니다.");
+	}
+}
