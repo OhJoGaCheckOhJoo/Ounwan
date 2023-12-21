c24cc7aee5d985283c5dda2c349acadacf5002ed
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-12-21 21:45:52 +0900

    danggun_Feat: #28 - danggun websocket service 구현

---
+++ src/main/java/com/ounwan/service/DanggunChatService.java
@@ -0,0 +1,80 @@
+package com.ounwan.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ounwan.dto.DanggunChatMessageDTO;
+import com.ounwan.entity.DanggunChatMessage;
+import com.ounwan.entity.DanggunChatRoom;
+import com.ounwan.repository.DanggunChatDAO;
+
+@Service
+public class DanggunChatService {
+	
+	@Autowired
+	DanggunChatDAO danggunChatDAO;
+	
+	public List<DanggunChatMessageDTO> selectDanggunChatList(Integer danggunNumber ,String seller, String buyer){
+//		일단 채팅방이 있는지 확인하자
+		List<String> danggunChatRoom = danggunChatDAO.selectRoomList();
+		System.out.println("방 있는지 = " + danggunChatRoom);
+		DanggunChatRoom makeChatRoom = new DanggunChatRoom();
+		String roomId = seller + buyer + danggunNumber;
+		makeChatRoom.setRoomId(roomId);
+		makeChatRoom.setDanggunNumber(danggunNumber);
+		makeChatRoom.setSeller(seller);
+		makeChatRoom.setBuyer(buyer);
+		if(!danggunChatRoom.contains(roomId)) {
+			int makeRoom = danggunChatDAO.makeRoom(makeChatRoom);
+		}
+		System.out.println("service roomID" + roomId);
+		return changeDTOlist(danggunChatDAO.selectDanggunChatList(roomId));
+	}
+	
+	public boolean insertMessage(DanggunChatMessageDTO danggunChatMessageDTO) {
+		int result = danggunChatDAO.insertMessage(changeEntity(danggunChatMessageDTO));
+		return (result > 0) ? true : false;
+	}
+	
+	public boolean deleteMessage(Integer messageId) {
+		int result = danggunChatDAO.deleteMessage(messageId);
+		return (result > 0) ? true : false;
+	}
+	
+	public int lastMessageId(String roomId) {
+		return danggunChatDAO.lastMessageId(roomId);
+	}
+	
+	public List<DanggunChatMessageDTO> changeDTOlist(List<DanggunChatMessage> danggunChat){
+		List<DanggunChatMessageDTO> changeList = new ArrayList<>();
+		for(DanggunChatMessage p : danggunChat) {
+			changeList.add(changeDTO(p));
+		}
+		return changeList;
+	}
+	
+	
+	public DanggunChatMessageDTO changeDTO(DanggunChatMessage danggunChat) {
+		return DanggunChatMessageDTO.builder()
+				.messageId(danggunChat.getMessageId())
+				.roomId(danggunChat.getRoomId())
+				.sender(danggunChat.getSender())
+				.message(danggunChat.getMessage())
+				.regdate(danggunChat.getRegdate())
+				.build();
+	}
+	
+	public DanggunChatMessage changeEntity(DanggunChatMessageDTO danggunChat) {
+		return DanggunChatMessage.builder()
+				.messageId(danggunChat.getMessageId())
+				.roomId(danggunChat.getRoomId())
+				.sender(danggunChat.getSender())
+				.message(danggunChat.getMessage())
+				.regdate(danggunChat.getRegdate())
+				.build();
+	}
+
+}
