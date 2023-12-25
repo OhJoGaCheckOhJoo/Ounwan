cd9b149061fb0dfd506c43e7482cfdcfbe568632
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-12-25 21:01:58 +0900

    danggun_Feat: #28 - somsomi chat DAO 구현

--- src/main/java/com/ounwan/repository/SomsomiChatDAO.java
+++ src/main/java/com/ounwan/repository/SomsomiChatDAO.java
@@ -6,7 +6,8 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.ounwan.entity.SomsomiChat;
+import com.ounwan.entity.SomsomiChatMessage;
+import com.ounwan.entity.SomsomiChatRoom;
 
 @Repository
 public class SomsomiChatDAO {
@@ -14,13 +15,30 @@ public class SomsomiChatDAO {
 	@Autowired
 	SqlSession sqlSession;
 	
-	private static final String NAMESPACE = "net.ounwan.chat.";
+	private static final String NAMESPACE = "net.ounwan.somsomiChat.";
 	
-	public List<SomsomiChat> selectChatList(){
-		return sqlSession.selectList(NAMESPACE + "selectChatList");
+	public List<String> selectRoomList(){
+		return sqlSession.selectList(NAMESPACE + "selectRoomList");
 	}
 	
-	public int insertMessage(SomsomiChat chat) {
+	public int makeRoom(SomsomiChatRoom makeChatRoom) {
+		return sqlSession.insert(NAMESPACE + "makeRoom", makeChatRoom);
+	}
+	
+	public List<SomsomiChatMessage> selectSomsomiChatList(String roomId){
+		return sqlSession.selectList(NAMESPACE + "selectSomsomiChatList", roomId);
+	}
+	
+	public int insertMessage(SomsomiChatMessage chat) {
 		return sqlSession.insert(NAMESPACE + "insertMessage", chat);
 	}
+
+	public int lastMessageId(String roomId) {
+		return sqlSession.selectOne(NAMESPACE + "lastMessageId", roomId);
+	}
+
+	public int deleteMessage(Integer messageId) {
+		return sqlSession.delete(NAMESPACE + "deleteMessage", messageId);
+	}
+
 }
