784b6a280de75f8a59fc72bb33db5b81fef38b36
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-12-25 21:02:11 +0900

    danggun_Feat: #28 - somsomi chat service 구현

--- src/main/java/com/ounwan/service/SomsomiChatService.java
+++ src/main/java/com/ounwan/service/SomsomiChatService.java
@@ -6,8 +6,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.ounwan.dto.SomsomiChatDTO;
-import com.ounwan.entity.SomsomiChat;
+import com.ounwan.dto.SomsomiChatMessageDTO;
+import com.ounwan.entity.SomsomiChatMessage;
+import com.ounwan.entity.SomsomiChatRoom;
 import com.ounwan.repository.SomsomiChatDAO;
 
 @Service
@@ -16,26 +17,50 @@ public class SomsomiChatService {
 	@Autowired
 	SomsomiChatDAO chatDAO;
 	
-	public List<SomsomiChatDTO> selectChatList() {
-		return changeDTOlist(chatDAO.selectChatList());
+	public List<SomsomiChatMessageDTO> selectSomsomiChatList(String clientId) {
+		List<String> somsomiChatRoom = chatDAO.selectRoomList();
+		SomsomiChatRoom makeChatRoom = new SomsomiChatRoom();
+//		일단 방이 존재하는지 체크
+		String roomId = clientId + clientId;
+		makeChatRoom.setRoomId(roomId);
+		makeChatRoom.setClientId(clientId);
+		makeChatRoom.setAdminId("admin");
+		
+		if(somsomiChatRoom.contains(makeChatRoom)) {
+			chatDAO.makeRoom(makeChatRoom);
+		}
+		
+		List<SomsomiChatMessageDTO> result = changeDTOlist(chatDAO.selectSomsomiChatList(roomId));
+		System.out.println(result);
+		return result;
 	}
 	
-	public boolean insertMessage(SomsomiChatDTO chatDTO) {
+	public boolean insertMessage(SomsomiChatMessageDTO chatDTO) {
 		int result = chatDAO.insertMessage(changeEntity(chatDTO));
 		return (result > 0) ? true : false;
 	}	
 	
-	public List<SomsomiChatDTO> changeDTOlist(List<SomsomiChat> chat){
-		List<SomsomiChatDTO> changeList = new ArrayList<>();
-		for(SomsomiChat p : chat) {
+	public int lastMessageId(String roomId) {
+		return chatDAO.lastMessageId(roomId);
+	}
+	
+	public Boolean deleteMessage(Integer messageId) {
+		int result = chatDAO.deleteMessage(messageId);
+		return (result > 0) ? true : false;
+	}
+
+	public List<SomsomiChatMessageDTO> changeDTOlist(List<SomsomiChatMessage> chat){
+		List<SomsomiChatMessageDTO> changeList = new ArrayList<>();
+		for(SomsomiChatMessage p : chat) {
 			changeList.add(changeDTO(p));
 		}
 		return changeList;
 	}
 	
 	
-	public SomsomiChatDTO changeDTO(SomsomiChat chat) {
-		return SomsomiChatDTO.builder()
+	public SomsomiChatMessageDTO changeDTO(SomsomiChatMessage chat) {
+		return SomsomiChatMessageDTO.builder()
+				.roomId(chat.getRoomId())
 				.messageId(chat.getMessageId())
 				.sender(chat.getSender())
 				.message(chat.getMessage())
@@ -43,8 +68,9 @@ public class SomsomiChatService {
 				.build();
 	}
 	
-	public SomsomiChat changeEntity(SomsomiChatDTO chat) {
-		return SomsomiChat.builder()
+	public SomsomiChatMessage changeEntity(SomsomiChatMessageDTO chat) {
+		return SomsomiChatMessage.builder()
+				.roomId(chat.getRoomId())
 				.messageId(chat.getMessageId())
 				.sender(chat.getSender())
 				.message(chat.getMessage())
