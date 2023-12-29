e4af13ad10bdac8fa780dc68ba03bf2b11c19580
youngee <80522538+seoyoung98@users.noreply.github.com>
2023-12-29 14:34:57 +0900

    dev_seoyoung2_Feat: #36 admin chat 수정

--- src/main/java/com/ounwan/controller/AdminChatRestController.java
+++ src/main/java/com/ounwan/controller/AdminChatRestController.java
@@ -1,10 +1,17 @@
 package com.ounwan.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ounwan.dto.SomsomiChatMessageDTO;
 import com.ounwan.service.AdminChatService;
+import com.ounwan.service.SomsomiChatService;
 
 @RequestMapping("/admin")
 @RestController
@@ -12,11 +19,24 @@ public class AdminChatRestController {
 	@Autowired
 	AdminChatService adminChatService;
 	
-//	@RequestMapping("/chat/list")
-//	public List<SomsomiChatMessageDTO> selectDanggunChatList(@RequestParam(name = "roomId") String roomId, @RequestParam(name = "clientId") String clientId, Model model) {
-//		List<SomsomiChatMessageDTO> result = adminChatService.selectSomsomiChatList(clientId, roomId);
-//		
-//		
-//		return result;
-//	}
+	@Autowired
+	SomsomiChatService chatService;
+	
+	@RequestMapping("/chat/list")
+	public List<SomsomiChatMessageDTO> selectDanggunChatList(@RequestParam(name = "roomId") String roomId, Model model) {
+		List<SomsomiChatMessageDTO> result = adminChatService.selectSomsomiChatList(roomId);
+		return result;
+	}
+	
+	@PostMapping("/chat/insertMessage")
+	public int insertMessage(SomsomiChatMessageDTO chatDTO) {
+		boolean result = chatService.insertMessage(chatDTO);
+		int lastMessageId = chatService.lastMessageId(chatDTO.getRoomId());
+		return lastMessageId;
+	}
+	
+	@PostMapping("/chat/delete")
+	public Boolean deleteMessage(Integer messageId) {
+		return chatService.deleteMessage(messageId);
+	}
 }
--- src/main/java/com/ounwan/service/AdminChatService.java
+++ src/main/java/com/ounwan/service/AdminChatService.java
@@ -8,11 +8,14 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ounwan.dto.SomsomiChatMessageDTO;
 import com.ounwan.dto.SomsomiChatRoomDTO;
 import com.ounwan.entity.Clients;
+import com.ounwan.entity.SomsomiChatMessage;
 import com.ounwan.entity.SomsomiChatRoom;
 import com.ounwan.repository.AdminChatDAO;
 import com.ounwan.repository.MyPageChatDAO;
+import com.ounwan.repository.SomsomiChatDAO;
 
 @Service
 public class AdminChatService {
@@ -23,8 +26,11 @@ public class AdminChatService {
 	@Autowired
 	MyPageChatDAO myPageChatDAO;
 	
+	@Autowired
+	SomsomiChatDAO chatDAO;
+	
 	public List<Map<String, Object>> getSomsomiRoomList() {
-		List<SomsomiChatRoomDTO> roomList = changeDTOlist(adminChatDAO.getSomsomiRoomList());
+		List<SomsomiChatRoomDTO> roomList = changeRoomDTOlist(adminChatDAO.getSomsomiRoomList());
 		
 		List<Map<String, Object>> clientInfo = new ArrayList<>();
 		
@@ -42,7 +48,31 @@ public class AdminChatService {
 		return clientInfo;
 	}
 	
-	public List<SomsomiChatRoomDTO> changeDTOlist(List<SomsomiChatRoom> chat){
+	public List<SomsomiChatMessageDTO> selectSomsomiChatList(String roomId) {
+		List<SomsomiChatMessageDTO> result = changeDTOlist(chatDAO.selectSomsomiChatList(roomId));
+		return result;
+	}
+	
+	public List<SomsomiChatMessageDTO> changeDTOlist(List<SomsomiChatMessage> chat){
+		List<SomsomiChatMessageDTO> changeList = new ArrayList<>();
+		for(SomsomiChatMessage p : chat) {
+			changeList.add(changeDTO(p));
+		}
+		return changeList;
+	}
+	
+	
+	public SomsomiChatMessageDTO changeDTO(SomsomiChatMessage chat) {
+		return SomsomiChatMessageDTO.builder()
+				.roomId(chat.getRoomId())
+				.messageId(chat.getMessageId())
+				.sender(chat.getSender())
+				.message(chat.getMessage())
+				.regdate(chat.getRegdate())
+				.build();
+	}
+	
+	public List<SomsomiChatRoomDTO> changeRoomDTOlist(List<SomsomiChatRoom> chat){
 		List<SomsomiChatRoomDTO> changeList = new ArrayList<>();
 		for(SomsomiChatRoom p : chat) {
 			changeList.add(changeRoomDTO(p));
